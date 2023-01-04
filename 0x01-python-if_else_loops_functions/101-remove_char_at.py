e5f7389a7a5f248734795b1f545e8d08af3ed3a6
firekoder <jasonpeterz.e1@gmail.com>
2023-01-04 06:27:45 +0100

    Correction 0x01 phython

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-# 101-remove_char_at.py
-
-
 def remove_char_at(str, n):
-
-if n < 0:
-        return (str)
-    return (str[:n] + str[n+1:])
+    newStr = ""
+    for i in range(len(str)):
+        if i == n:
+            continue
+        else:
+            newStr += str[i]
+    return newStr
--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-# 102-magic_calculation.py
-
-
-
 def magic_calculation(a, b, c):
-    
     if a < b:
-        return (c)
-    if c > b:
-        return (a + b)
-    return (a*b - c)
+        return c
+    elif c > b:
+        return a + b
+
+    return a * b - c
