5be461706590044d696574d104635f8e8ef1cd71
firekoder <jasonpeterz.e1@gmail.com>
2023-01-06 02:45:53 +0100

    Python import modules correction

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 from sys import argv
-add = 0
-for s in argv[1:]:
-    add += int(s)
-print("{:d}".format(add))
+if __name__ == "__main__":
+    count = len(argv)
+    add = 0
+    if count == 1:
+        print("{}".format(add))
+    else:
+        for i in range(1, count):
+            add += int(argv.__getitem__(i))
+        print("{}".format(add))
